c41cb8f1041e902c79c97addd0ea541cf30fd032
Utkarsh Sharma <utkarshsharma@Utkarshs-MacBook-Air.local>
2024-08-05 10:30:51 +0530

    Added Protected routes and vercel.json file

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,13 +1,18 @@
 
-import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import GenrePage from "./pages/GenrePage";
 import CarouselPage from "./pages/CarouselPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import DashboardPage from "./pages/DashboardPage";
 import MoviePage from "./pages/MoviePage";
+import { AppContext } from "./context/AppContext";
 
+const ProtectedRoute = ({ children }) => {
+	const { user } = useContext(AppContext)
+	return user ? children : <Navigate to="/register" />
+}
 function App() {
 	return (
 		<BrowserRouter>
@@ -15,14 +20,30 @@ function App() {
 				<Route path="/" element={<RegisterPage />} />
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/home" element={<RegisterPage />} />
-				<Route path="/genres" element={<GenrePage />} />
-				<Route path="/carousel" element={<CarouselPage />} />
-				<Route path="/dashboard" element={<DashboardPage />} />
-				<Route path="/movies" element={<MoviePage />} />
+				<Route path="/genres" element={
+					<ProtectedRoute>
+						<GenrePage />
+					</ProtectedRoute>} />
+				<Route path="/carousel" element={
+					<ProtectedRoute>
+						<CarouselPage />
+					</ProtectedRoute>
+				} />
+				<Route path="/dashboard" element={
+					<ProtectedRoute>
+						<DashboardPage />
+					</ProtectedRoute>
+				} />
+				<Route path="/movies" element={
+					<ProtectedRoute>
+						<MoviePage />
+					</ProtectedRoute>
+
+				} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</BrowserRouter>
-    
+
 	);
 }
 
